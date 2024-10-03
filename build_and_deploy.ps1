ba61f9ecb37906f00c3c5b5dfc4fc123d53513e5
KoenVA <koenvanaken1999@gmail.com>
2024-10-03 21:41:22 +0200

    Make two pages

---
+++ build_and_deploy.ps1
@@ -0,0 +1,8 @@
+try {
+    cd "C:/Users/Koenv/Documents/Projects/Website/Koenraad007.github.io"
+    ng build --base-href="https://koenraad007.github.io/"
+    npx angular-cli-ghpages --dir=dist/website
+}
+catch {
+    "Error occurred"
+}
