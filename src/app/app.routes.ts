2e175f13c0416a68588a014ca4f3739f487746b1
KoenVA <koenvanaken1999@gmail.com>
2024-10-17 16:40:01 +0200

    Fix default route empty page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'contact', component: ContactComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
 ];
